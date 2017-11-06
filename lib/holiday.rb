4a854361b7efecf85b92245e79b97de4492ccf8c
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-06 17:57:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,8 +18,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash(season, holiday)
-  
+  holiday_hash.each do |season, holiday|
+    
+  end
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
